949d0524a4d7b2a1ddb272a67b2b85ef546abe2b
Julia Pięt <julia.piet@studenci.collegiumwitelona.pl>
2023-10-28 10:37:21 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Lab03_zad4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab03_zad4.dir/main.c.obj"
+  "CMakeFiles/Lab03_zad4.dir/main.c.obj.d"
+  "Lab03_zad4.exe"
+  "Lab03_zad4.exe.manifest"
+  "Lab03_zad4.pdb"
+  "libLab03_zad4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab03_zad4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
